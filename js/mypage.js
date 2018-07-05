d4380118aaaad27ab8456cdf6b1dd12bb368864b
Jennifer Walker <dataqueen16@gmail.com>
2018-07-04 22:46:51 -0500

    cosmetic changes to all 3 pages

--- js/mypage.js
+++ js/mypage.js
@@ -1,8 +1,6 @@
 (function () {
     "use strict";
 
-
-
     $('#toggle1, #toggle2, #toggle3, #toggle4, #toggle5').click(function(event) {
         event.preventDefault();
         $(this).parent().next().toggleClass('invisible');
