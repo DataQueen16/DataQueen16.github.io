74a0f03982bf60661ff4021cafa07ea12c95b7df
Dataqueen16 <dataqueen16@gmail.com>
2019-11-24 13:45:26 -0600

    Updated personal data for skills and work experience

--- js/mypage.js
+++ js/mypage.js
@@ -1,7 +1,7 @@
 (function () {
     "use strict";
 
-    $('#toggle1, #toggle2, #toggle3, #toggle4, #toggle5').click(function(event) {
+    $('#toggle1, #toggle2, #toggle3, #toggle4, #toggle5, #toggle6').click(function(event) {
         event.preventDefault();
         $(this).parent().next().toggleClass('invisible');
     });
